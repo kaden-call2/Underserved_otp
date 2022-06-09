f8cdc584b60019964f4a0cba1353e5e89b0f8afd
Call <kaden.call@gainwelltechnologies.com>
2022-06-09 14:35:18 -0600

    added rates graph func

--- src/func1.py
+++ src/func1.py
@@ -30,8 +30,8 @@ zip_map = {'Samoa': ['96799'],
             'Puerto Rico': ['00' + x for x in [str(y) for y in list(range(601, 989))]]}
 
 #Read in the zip codes
-# all_zips = gpd.read_file('https://www2.census.gov/geo/tiger/TIGER2019/ZCTA5/tl_2019_us_zcta510.zip')
-all_zips = gpd.read_file('data/zip_codes.shp')
+all_zips = gpd.read_file('https://www2.census.gov/geo/tiger/TIGER2019/ZCTA5/tl_2019_us_zcta510.zip')
+# all_zips = gpd.read_file('data/zip_codes.shp')
 all_zips = all_zips.rename(columns={'ZCTA5CE10':'ZIP'})
 
 
@@ -46,9 +46,9 @@ def plot_rates():
     for year in range(2013, 2020):
         rates_year.append(rates_data[rates_data['Year'] == year])
     rates_2019 = rates_year[-1]
-    print(rates_2019['Prscrbr_Geo_Cd'])
+    # print(rates_2019['Prscrbr_Geo_Cd'])
     rates_2019['ZIP'] = format_rates_zip_codes(rates_2019['Prscrbr_Geo_Cd'])
-    print(rates_2019['ZIP'])
+    # print(rates_2019['ZIP'])
 
 
 
@@ -61,7 +61,7 @@ def plot_rates():
     Alaska = states.loc[states['STATEFP'] == '02']
     Hawaii = states.loc[states['STATEFP'] == '15']
     states = states[[s not in invalid_FIPS for s in states['STATEFP']]]
-    # state_boundary_map = states.boundary.plot(figsize=(12,9), color='Black', linewidth=.25)
+    state_boundary_map = states.boundary.plot(figsize=(12,9), color='Black', linewidth=.25)
 
 
     pd.set_option('display.max_columns', None)
@@ -70,12 +70,15 @@ def plot_rates():
     invalid = zip_map['Mariana'] + zip_map['Guam'] + zip_map['Alaska'] + zip_map['Samoa'] + zip_map['Hawaii'] + zip_map['Virgin_isles'] + zip_map['Puerto Rico']
     mask = [z not in invalid for z in all_zips['ZIP']]
     cont_US_zips = all_zips[mask]
-    joined = all_zips.join(rates_2019.set_index(['ZIP'], verify_integrity=True), on=['ZIP'], how='left')
-    print(joined)
+
+    mask = [z not in invalid for z in rates_2019['ZIP']]
+    filtered_2019_rates = rates_2019[mask]
+    joined = cont_US_zips.join(filtered_2019_rates.set_index(['ZIP'], verify_integrity=True), on=['ZIP'], how='left')
+    # print(joined)
     # print(rates_2019['Prscrbr_Geo_Cd'])
     # print(cont_US_zips)
-    # cont_US_zips.plot(ax=state_boundary_map, column='Opioid_Prscrbng_Rate')
-    # plt.show()
+    joined.plot(ax=state_boundary_map, column='Opioid_Prscrbng_Rate', legend=True, cmap='Blues')
+    plt.show()
 
 
 
