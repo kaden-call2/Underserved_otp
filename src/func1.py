a68bc442b5b04e9651dd4da1cf91028db45bb6b3
Call <kaden.call@gainwelltechnologies.com>
2022-08-11 14:37:27 -0600

    final changes

--- src/func1.py
+++ src/func1.py
@@ -260,8 +260,16 @@ def plot_rates(states_geo_data, zip_geo_data, year_rates_data, provider_data, pl
     if plot_column == 'Tot_Opioid_Clms':
         plt.title('Total Opioid Claims: ' + str(plot_states))
 
+<<<<<<< HEAD
     else:
         plt.title(plot_column + ': ' + str(plot_states))
+=======
+    if plot_column == 'Tot_Opioid_Clms':
+        plt.title('Total Opioid Claims: ' + str(plot_states))
+
+    else:
+        plt.title(plot_column + ' state: ' + str(plot_states))
+>>>>>>> 0a7a8d55e3355e5cb93d1da88b8866c3a25e4351
     plt.show()
 
 
