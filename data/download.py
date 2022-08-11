8c358e36125295a3eda51832fee9a8cb022f1522
Call <kaden.call@gainwelltechnologies.com>
2022-08-11 14:59:03 -0600

    final changes

--- data/download.py
+++ data/download.py
@@ -1,18 +1,9 @@
 import geopandas as gpd
-import svi_data
 
-# Code to download the geometric files for the state lines and zip code lines as well as the SVI data.
+# Code to download the geometric files for the state lines and zip code lines
 
 zip_codes = gpd.read_file('https://www2.census.gov/geo/tiger/TIGER2019/ZCTA5/tl_2019_us_zcta510.zip')
 zip_codes.to_file('data/zip_shapes/zip_geo_data.shp')
 
 states = gpd.read_file('https://www2.census.gov/geo/tiger/TIGER2019/STATE/tl_2019_us_state.zip')
 states.to_file('data/state_shapes/states_geo_data.shp')
-
-
-# Need to sign up for your own key
-with open('data/api_key.txt', 'r') as infile:
-    key = infile.read()
-# Download the data from census API
-svi_zips = svi_data.get_svi(key,'zip',2019)
-open('data/ZipData.csv', 'wb').write(svi_zips)
